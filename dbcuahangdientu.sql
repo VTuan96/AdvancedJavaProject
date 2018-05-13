d27c152e4ecb523ec82448c869594e1278c74f78
VTuan96 <vutuan.bkset@gmail.com>
2018-05-13 16:10:40 +0700

    Add files via upload

--- dbcuahangdientu.sql
+++ dbcuahangdientu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 12, 2018 lúc 10:23 AM
+-- Thời gian đã tạo: Th5 13, 2018 lúc 11:08 AM
 -- Phiên bản máy phục vụ: 10.1.31-MariaDB
 -- Phiên bản PHP: 7.2.3
 
@@ -32,20 +32,18 @@ CREATE TABLE `tblinhkien` (
   `Id_linh_kien` int(11) NOT NULL,
   `Ten_linh_kien` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `Hinh_anh` text COLLATE utf8_unicode_ci NOT NULL,
-  `Ngay_nhap` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `Gia_nhap` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `Gia_ban` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `Loai_linh_kien_id` int(11) NOT NULL,
-  `Vi_tri_linh_kien_id` int(11) NOT NULL
+  `Vi_tri_linh_kien_id` int(11) NOT NULL,
+  `Id_gia_linh_kien` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `tblinhkien`
 --
 
-INSERT INTO `tblinhkien` (`Id_linh_kien`, `Ten_linh_kien`, `Hinh_anh`, `Ngay_nhap`, `Gia_nhap`, `Gia_ban`, `Loai_linh_kien_id`, `Vi_tri_linh_kien_id`) VALUES
-(5, 'Điện trở 10 ôm', 'C:UsersdtvtaPicturesScreenshots	ải xuống.jpg', '2018/05/11', '20000', '50000', 5, 8),
-(6, 'Bóng đèn 100w', 'C:UsersdtvtaPicturesScreenshotsScreenshot (1).png', '2018/05/11', '30000', '50000', 25, 2);
+INSERT INTO `tblinhkien` (`Id_linh_kien`, `Ten_linh_kien`, `Hinh_anh`, `Loai_linh_kien_id`, `Vi_tri_linh_kien_id`, `Id_gia_linh_kien`) VALUES
+(5, 'Điện trở 10 ôm', 'C:UsersdtvtaPicturesScreenshots	ải xuống.jpg', 5, 8, 0),
+(6, 'Bóng đèn 100w', 'C:UsersdtvtaPicturesScreenshotsScreenshot (1).png', 25, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -55,25 +53,26 @@ INSERT INTO `tblinhkien` (`Id_linh_kien`, `Ten_linh_kien`, `Hinh_anh`, `Ngay_nha
 
 CREATE TABLE `tbloailinhkien` (
   `Id_loai_linh_kien` int(11) NOT NULL,
-  `Ten_loai_linh_kien` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+  `Ten_loai_linh_kien` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Id_loai_san_pham` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `tbloailinhkien`
 --
 
-INSERT INTO `tbloailinhkien` (`Id_loai_linh_kien`, `Ten_loai_linh_kien`) VALUES
-(5, 'điện trở'),
-(24, 'abc'),
-(25, 'bóng đèn'),
-(26, 'điện trở'),
-(27, 'biến trở'),
-(28, 'abe'),
-(29, 'aw'),
-(30, 'awe'),
-(31, 'abcd'),
-(32, 'w'),
-(33, 'ae');
+INSERT INTO `tbloailinhkien` (`Id_loai_linh_kien`, `Ten_loai_linh_kien`, `Id_loai_san_pham`) VALUES
+(5, 'điện trở', 0),
+(24, 'abc', 0),
+(25, 'bóng đèn', 0),
+(26, 'điện trở', 0),
+(27, 'biến trở', 0),
+(28, 'abe', 0),
+(29, 'aw', 0),
+(30, 'awe', 0),
+(31, 'abcd', 0),
+(32, 'w', 0),
+(33, 'ae', 0);
 
 -- --------------------------------------------------------
 
